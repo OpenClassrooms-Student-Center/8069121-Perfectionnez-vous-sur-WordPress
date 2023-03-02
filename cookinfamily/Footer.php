8ed78c37432570ac770c01ff2938157b50e177c9
Aurélien Boulard <aurelien.boulard@it-consultis.com>
2023-03-02 23:03:33 +0100

    Add bootstrap design for the theme

--- cookinfamily/Footer.php
+++ cookinfamily/Footer.php
@@ -1,3 +1,8 @@
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">© <?php echo date('Y') . ' ' . __('CookInFamily', 'cookinfamily'); ?></p>
+      </footer>
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
--- cookinfamily/header.php
+++ cookinfamily/header.php
@@ -3,7 +3,18 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>CookInFamily</title>
+    <title><?php _e('CookInFamily', 'cookinfamily'); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
   </head>
   <body>
+    <div class="container">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <img class="bi me-2" width="40" height="32" src="assets/images/" alt="<?php _e('CookInFamily', 'cookinfamily'); ?>" />
+          <span class="fs-4"><?php _e('CookInFamily', 'cookinfamily'); ?></span>
+        </a>
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="/" class="nav-link active" aria-current="page"><?php _e('Accueil', 'cookinfamily'); ?></a></li>
+          <li class="nav-item"><a href="https://github.com/OpenClassrooms-Student-Center/8069121-Perfectionnez-vous-sur-WordPress" target="_blank" class="nav-link"><?php _e('Cours OpenClassRooms', 'cookinfamily'); ?></a></li>
+        </ul>
+      </header>

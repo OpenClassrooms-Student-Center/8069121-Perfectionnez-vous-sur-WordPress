e961ff739256ac9af2f7f4dab4d4d314968a5d84
Aurélien Boulard <aurelien.boulard@it-consultis.com>
2023-03-29 15:25:55 +0200

    Update a bit the form

--- cookinfamily/functions.php
+++ cookinfamily/functions.php
@@ -1,19 +1,18 @@
 <?php
     function cookinfamily_add_admin_pages() {
-        add_menu_page(__('CookInFamily', 'cookinfamily'), __('CookInFamily', 'cookinfamily'), 'manage_options', 'cookinfamily-settings', 'cookinfamily_theme_settings', 'dashicons-admin-settings', 60); 
+        add_menu_page(__('Paramètres du thème CookInFamily', 'cookinfamily'), __('CookInFamily', 'cookinfamily'), 'manage_options', 'cookinfamily-settings', 'cookinfamily_theme_settings', 'dashicons-admin-settings', 60); 
     }
 
     function cookinfamily_theme_settings() {
-        echo '<h1>'.__('Paramètres du thème CookInFamily', 'cookinfamily').'</h1>';
+        echo '<h1>'.esc_html( get_admin_page_title() ).'</h1>';
         echo '<form action="options.php" method="post" name="cookinfamily_settings">';
         echo '<div>';
   
         settings_fields('cookinfamily_settings_fields');
         do_settings_sections('cookinfamily_settings_section');
-  
+        submit_button();
+
         echo '</div>';
-        echo '<br />';
-        echo '<input name="submit" class="button button-primary" type="submit" value="'.__('Enregistrer', 'cookinfamily').'" />';
         echo '</form>';
     }
 
@@ -57,13 +56,46 @@
       echo '<input name="cookinfamily_settings_field_phone_number" type="text" value="'.$value.'" />';
      }
      
-     function cookinfamily_settings_field_email_output() {
+    function cookinfamily_settings_field_email_output() {
       $value = get_option('cookinfamily_settings_field_email');
 
       echo '<input name="cookinfamily_settings_field_email" type="text" value="'.$value.'" />';
-     }
+    }
+
+    function cookinfamily_register_custom_post_types() {
+			$labels_ingredient = array(
+				'menu_name'             => __('Ingrédients', 'cookinfamily'),
+				'name_admin_bar'        => __('Ingrédient', 'cookinfamily'),
+	      'add_new_item'          => __('Ajouter un nouvel ingrédient', 'cookinfamily'),
+	      'new_item'              => __('Nouvel ingrédient', 'cookinfamily'),
+	      'edit_item'             => __('Modifier l\'ingrédient', 'cookinfamily'),
+			);
+
+			$args_ingredient = array(
+				'label'                 => __('Ingrédients', 'cookinfamily'),
+				'description'           => __('Ingrédients', 'cookinfamily'),
+				'labels'                => $labels_ingredient,
+				'supports'              => array('title', 'thumbnail', 'excerpt', 'editor'),
+				'hierarchical'          => false,
+				'public'                => true,
+				'show_ui'               => true,
+				'show_in_menu'          => true,
+				'menu_position'         => 40,
+				'show_in_admin_bar'     => true,
+				'show_in_nav_menus'     => true,
+				'can_export'            => true,
+				'has_archive'           => true,
+				'exclude_from_search'   => false,
+				'publicly_queryable'    => true,
+				'capability_type'       => 'post',
+				'menu_icon'   					=> 'dashicons-drumstick',
+			);
+
+			register_post_type('cif_ingredient', $args_ingredient);
+		}
 
     /***** Actions *****/
     add_action('admin_menu', 'cookinfamily_add_admin_pages', 10);
     add_action('admin_init', 'cookinfamily_settings_register');
+    add_action('init', 'cookinfamily_register_custom_post_types', 11);
 ?>
